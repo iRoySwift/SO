6fe646260ffcdcbcba880230fc52377b8d622370
Roy <iRoySwift@iCloud.com>
2026-03-01 18:08:35 +0800

    Theme

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,16 @@
+import {
+    DarkTheme,
+    DefaultTheme,
+    ThemeProvider,
+} from "@react-navigation/native";
 import { Stack } from "expo-router";
+import { useColorScheme } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  const colorScheme = useColorScheme(); // Force re-render on color scheme change. The theme is provided by the nearest `AppearanceProvider` component in the tree.
+  return (
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <Stack />
+    </ThemeProvider>
+  );
 }
